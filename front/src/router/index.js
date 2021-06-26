fa69e87437cdb81da3325c4b7a7bbb9a968b0e8b
Payhaev17 <ran777dom@gmail.com>
2021-06-26 15:14:07 +0300

    changes

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,6 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-const routes = [];
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: () => import("@/views/Home.vue"),
+  },
+  {
+    path: "/auth",
+    name: "Auth",
+    component: () => import("@/views/Auth.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
---
+++ front/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Home</div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  mounted() {
+    fetch("http://back:85/");
+  },
+};
+</script>
