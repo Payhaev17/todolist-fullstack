8fba6c95bc459db299177697a0a775094d5826b0
Payhaev17 <ran777dom@gmail.com>
2021-07-07 23:06:34 +0300

    changes

--- front/src/main.js
+++ front/src/main.js
@@ -5,8 +5,6 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
-import "@/css/vars.css";
-import "@/css/helpers.css";
 import "@/css/index.css";
 import "material-design-icons/iconfont/material-icons.css";
 
