7ab3798dd7c0624788ac8c725729df8c524a8701
Payhaev17 <ran777dom@gmail.com>
2021-07-15 21:15:00 +0300

    changes

--- front/src/store/todos.js
+++ front/src/store/todos.js
@@ -35,20 +35,6 @@ export default {
         }
       );
 
-      // const todos = [
-      //   { id: 1, title: "Title", text: "text task a", succ: 0 },
-      //   { id: 2, title: "Title", text: "text task a", succ: 0 },
-      //   { id: 3, title: "Title", text: "text task a", succ: 0 },
-      //   { id: 4, title: "Title", text: "text task a", succ: 0 },
-      //   { id: 5, title: "Title", text: "text task a", succ: 0 },
-      //   { id: 6, title: "Title", text: "text task a", succ: 0 },
-      //   { id: 7, title: "Title", text: "text task a", succ: 0 },
-      //   { id: 8, title: "Title", text: "text task a", succ: 0 },
-      //   { id: 9, title: "Title", text: "text task a", succ: 0 },
-      //   { id: 10, title: "Title", text: "text task a", succ: 0 },
-      //   { id: 11, title: "Title", text: "text task a", succ: 0 },
-      // ];
-
       const todos = await res.json();
 
       if (todos instanceof Array) {
